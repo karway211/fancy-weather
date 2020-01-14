1cb1295a088b79573c9d113cb9cb1046f3e0a83d
Andrey K <karway211@gmail.com>
2020-01-14 23:48:45 +0300

    feat: add code

--- src/app/weather-next.js
+++
@@ -1,9 +0,0 @@
-const weaterNextF = (tMax, tMin) => {
-    return Math.round((tMax + tMin) / 2);
-}
-
-const weaterNextC = (tMax, tMin) => {
-    return Math.round(((tMax + tMin) / 2 - 32) * 5 / 9);
-}
-
-export { weaterNextF, weaterNextC }
